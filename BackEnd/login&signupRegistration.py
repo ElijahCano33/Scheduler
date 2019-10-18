5715c2bdc027ff500c992a1ff59802cfffaf90cc
LeKinchi <32843930+LeKinchi@users.noreply.github.com>
2019-10-18 19:08:41 -0400

    edited the register endpoint to connect to the db

--- BackEnd/login&signupRegistration.py
+++ BackEnd/login&signupRegistration.py
@@ -1,11 +1,11 @@
-from flask import Flask, session, redirect, url_for, escape, request, render_template
-from hashlib import md5
+from flask import Flask, request
+from hashlib import sha1
 import mysql.connector
-from mysql.connector import Error
-
+import json
 
 app = Flask(__name__)
-connection = mysql.connector.connect(host='scheduler-mysql-db.cxe7niamrusn.us-west-2.rds.amazonaws.com', database='Scheduler', user='admin_Scheduler', password='82h20kfaCrn05EKpEDrh')
+connection = mysql.connector.connect(host='scheduler-mysql-db.cxe7niamrusn.us-west-2.rds.amazonaws.com',
+                                     database='Scheduler', user='admin_Scheduler', password='82h20kfaCrn05EKpEDrh')
 cursor = connection.cursor()
 
 
@@ -13,47 +13,55 @@ cursor = connection.cursor()
 def connection_test():
     if connection.is_connected():
         db_info = connection.get_server_info()
-        print("Connected to MySQL Server version ", db_info)
         cursor.execute("select database();")
         record = cursor.fetchone()
-        print("Your connected to database: ", record)
+        return f'''
+        Connected to MySQL Server version {db_info}
+        Your connected to database: {record}
+        '''
     else:
-        print("Database is having an error")
+        return "Database is having an error"
 
 
 @app.route("/api/register", methods=['GET', 'POST'])
 def register_():
-    response = {}
+    response = dict()
     data = request.get_json()
     email = data['email']
-    password = data['password']
-    display = data['display_name']
-    imei = data["imei"]
-    if request.method == 'POST' and 'email' in request.form and 'password' in request.form:
-        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (email, password))
+    passphrase = data['password']
+    user_name = data['user_name']
+    imei = data['imei']
+    first_name = data['first_name']
+    last_name = data['last_name']
+
+    if request.method == 'POST' and 'email' in data and 'password' in data:
+        cursor.execute(f'SELECT * FROM Scheduler.users WHERE username = "{user_name}" or email = "{email}"')
         account = cursor.fetchone()
-        if not account[0]:
-            cursor.execute("insert into Scheduler.users (username,email,password,mac_address,"
+        if account:
+            response['status'] = 'username or email already exist'
+        else:
+            cursor.execute( f'insert into Scheduler.users (username, email, password, mac_address,first_name, last_name) values ("{user_name}", "{email}", "{passphrase}", "{imei}", "{first_name}", "{last_name}")')
+            response['status'] = "Successfull"
+    return response
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     response = {}
     if request.method == 'POST':
-        email  = request.get_json()['email']
-        cursor.execute("SELECT COUNT(1) FROM users WHERE email = %s". (email)
+        email = request.get_json()['email']
+        cursor.execute("SELECT COUNT(1) FROM users WHERE email = %s", (email))
         if not cursor.fetchone()[0]:
             response['status'] = 'invalid email or email not present in db'
-			return response
-        password  = request.get_json()['password']
+            return response
+        password = request.get_json()['password']
         cursor.execute("SELECT password FROM users WHERE name = %s", (email))
         for row in cursor.fetchall():
             if md5(password).hexdigest() == row[0]:
                 response['status'] = 'Sucessful login'
                 response['status'] = 'invalid password'
-                retrurn response
+                return response
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
