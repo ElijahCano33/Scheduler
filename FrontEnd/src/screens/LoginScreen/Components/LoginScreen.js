e791d283638b30e6d8a7c46e987b3c83318bd604
elijahcano33 <elijah.c104777@gmail.com>
2019-10-19 11:52:08 -0400

    Login page styles finished for Android

--- FrontEnd/src/screens/LoginScreen/Components/LoginScreen.js
+++ FrontEnd/src/screens/LoginScreen/Components/LoginScreen.js
@@ -1,13 +1,42 @@
-import React, {Component} from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TextInput,
+  TouchableOpacity
+} from 'react-native';
 import styles from '../Styles/LoginScreenStyles.js';
 
-export default class LoginScreen extends Component{
-    render(){
-        return (
-            <View style={styles.screenColor}>
-                
-            </View>
-        );
-    }
+export default class LoginScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      
+      <Image
+        style={styles.logo}
+        source={require('../../../../pics/scheduler.png')}
+       />
+      
+      <TextInput
+            placeholder="  email"
+            placeholderTextColor='#000000'
+            style={styles.input1}
+          />
+      <TextInput
+            placeholder="  password"
+            placeholderTextColor='#000000'
+            style={styles.input2}
+          />
+      <TouchableOpacity style={styles.buttonContainer1}>
+        <Text style={styles.buttonText}>LOGIN</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.buttonContainer2}>
+        <Text style={styles.buttonText}>SIGN UP</Text>
+      </TouchableOpacity>
+      </View>
+    );
+  }
 }
--- FrontEnd/src/screens/LoginScreen/Styles/LoginScreenStyles.js
+++ FrontEnd/src/screens/LoginScreen/Styles/LoginScreenStyles.js
@@ -1,10 +1,58 @@
 import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
-    screenColor: {
-        backgroundColor: '#7F00FF',
-        flex:1,
-        justifyContent: 'center'
-        
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#40e0d0'
+    },
+  
+    logo: {
+      height: 160,
+      width: 320,
+      top: '-30%',
+    },
+  
+    input1: {
+      height: 40,
+      width: 250,
+      backgroundColor: '#FFFFFF',
+      marginBottom: 20,
+      top: '40%',
+      position: 'absolute'
+    },
+
+    input2: {
+        height: 40,
+        width: 250,
+        backgroundColor: '#FFFFFF',
+        marginBottom: 20,
+        top: '-7%', 
+      },
+  
+    buttonContainer1: {
+      width: 120,
+      backgroundColor: '#000000',
+      paddingHorizontal: 30,
+      textAlign: 'left',
+      left: '-16.5%',
+      top: '-8%'
+    },
+
+    buttonContainer2: {
+        width: 120,
+        backgroundColor: '#000000',
+        paddingHorizontal: 30,
+        textAlign: 'left',
+        top: '-10.72%',
+        left: '16.5%'
+      },
+  
+    buttonText: {
+      textAlign: 'center',
+      color: '#FFFFFF',
+      fontWeight: '700'
     }
-});
+  });
+  
