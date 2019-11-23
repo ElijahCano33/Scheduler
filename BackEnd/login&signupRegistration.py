8174a2d93e03db66487d243ba9df81aea60df99e
Elijah <35966941+ElijahCano33@users.noreply.github.com>
2019-11-23 14:37:56 -0500

    Update login&signupRegistration.py

--- BackEnd/login&signupRegistration.py
+++ BackEnd/login&signupRegistration.py
@@ -1,13 +1,11 @@
 from flask import Flask, request
 from hashlib import sha1
 import mysql.connector
-import json
 
 app = Flask(__name__)
 database = mysql.connector.connect(host='scheduler-mysql-db.cxe7niamrusn.us-west-2.rds.amazonaws.com',
-                                     database='Scheduler', user='admin_Scheduler', password='82h20kfaCrn05EKpEDrh')
+                                   database='Scheduler', user='admin_Scheduler', password='82h20kfaCrn05EKpEDrh')
 cursor = database.cursor()
-
 
 @app.route('/api/connection-test')
 def connection_test():
@@ -23,14 +21,14 @@ def connection_test():
         return "Database is having an error"
 
 
-@app.route("/api/register", methods=['GET', 'POST'])
+@app.route("/api/register", methods=['POST'])
 def register_():
     response = dict()
     data = request.get_json()
     email = data['email']
     passphrase = data['password']
     user_name = data['user_name']
-    imei = data['imei']
+    #imei = data['imei']
     first_name = data['first_name']
     last_name = data['last_name']
 
@@ -38,31 +36,55 @@ def register_():
         cursor.execute(f'SELECT * FROM Scheduler.users WHERE username = "{user_name}" or email = "{email}"')
         account = cursor.fetchone()
         if account:
-            response['status'] = 'username or email already exist'
+            response['status'] = False
+            response['status_info'] = 'username or email already exist'
         else:
-            cursor.execute( f'insert into Scheduler.users (username, email, password, mac_address,first_name, last_name) values ("{user_name}", "{email}", "{passphrase}", "{imei}", "{first_name}", "{last_name}")')
+            cursor.execute(
+                f'insert into Scheduler.users (username, email, password, first_name, last_name) values ("{user_name}", "{email}", "{passphrase}",  "{first_name}", "{last_name}")')
             database.commit()
-            response['status'] = "Successfull"
+            response['status'] = True
+            response['status_info'] = 'account created successfully'
     return response
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/api/login', methods=['POST'])
 def login():
     response = {}
     if request.method == 'POST':
         email = request.get_json()['email']
-        cursor.execute("SELECT COUNT(1) FROM users WHERE email = %s", (email))
+        cursor.execute(f"SELECT COUNT(1) FROM Scheduler.users WHERE email = '{email}'")
         if not cursor.fetchone()[0]:
-            response['status'] = 'invalid email or email not present in db'
-            return response
-        password = request.get_json()['password']
-        cursor.execute("SELECT password FROM users WHERE name = %s", (email))
-        for row in cursor.fetchall():
-            if md5(password).hexdigest() == row[0]:
-                response['status'] = 'Sucessful login'
-                response['status'] = 'invalid password'
-                return response
+            response['status']= False
+            response['status_info'] = 'invalid email or email not present in db'
+            print(response)
+        else:
+            password = request.get_json()['password']
+            cursor.execute(f'SELECT password FROM Scheduler.users WHERE email = "{email}"')
+            results = cursor.fetchall()
+            if len(results) > 0:
+                response['status']= False
+                response['status_info'] = 'invalid password'
+            for row in results:
+                if encrypt_password(password) == row[0]:
+                    response['status']= True
+                    response['status_info'] = 'Login attempt was Successful'
+                    response["authentication token"] = generate_authorization_token()
+    else:
+        response["status"] = False
+        response["status_info"] = "invalid request type"
+
+    return response
+
+
+def encrypt_password(password_unencrypted):
+    hashed_password = "jk;hs;jh"
+    return password_unencrypted
+
+
+def generate_authorization_token():
+    return "laughing"
 
 
 if __name__ == '__main__':
     app.run(debug=True)
+
