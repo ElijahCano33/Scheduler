9fa55de6a940fd08f3d1312aebb6f7a37273ff06
elijahcano33 <elijah.c104777@gmail.com>
2020-01-06 13:09:49 -0500

    Successfully separated routes into subdirectory

--- BackEnd/Routes/login.py
+++ BackEnd/Routes/login.py
@@ -1,3 +1,4 @@
+from flask import Flask, Blueprint
 from flask import Flask, request
 from hashlib import sha1
 from passlib.hash import argon2
@@ -5,58 +6,14 @@ import secrets
 import bcrypt
 import mysql.connector
 
-app = Flask(__name__)
 database = mysql.connector.connect(host='scheduler-mysql-db.cxe7niamrusn.us-west-2.rds.amazonaws.com',
                                    database='Scheduler', user='admin_Scheduler', password='82h20kfaCrn05EKpEDrh')
 
-                  
-
 cursor = database.cursor()
 
-@app.route('/api/connection-test')
-def connection_test():
-    if database.is_connected():
-        db_info = database.get_server_info()
-        cursor.execute("select database();")
-        record = cursor.fetchone()
-        return f'''
-        Connected to MySQL Server version {db_info}
-        Your connected to database: {record}
-        '''
-    else:
-        return "Database is having an error"
+log = Blueprint('login', __name__)
 
-
-@app.route("/api/register", methods=['POST'])
-def register_():
-    response = dict()
-    data = request.get_json()
-    email = data['email']
-    passphrase = data['password']
-    salt = generate_salt_string()
-    newPassPhrase = encrypt_password(passphrase, salt)
-    salt = salt.decode("utf-8")
-    user_name = data['user_name']
-    #imei = data['imei']
-    first_name = data['first_name']
-    last_name = data['last_name']
-
-    if request.method == 'POST' and 'email' in data and 'password' in data:
-        cursor.execute(f'SELECT * FROM Scheduler.users WHERE username = "{user_name}" or email = "{email}"')
-        account = cursor.fetchone()
-        if account:
-            response['status'] = False
-            response['status_info'] = 'username or email already exist'
-        else:
-            cursor.execute(
-                f'insert into Scheduler.users (username, email, password, salts, first_name, last_name) values ("{user_name}", "{email}", "{newPassPhrase}", "{salt}", "{first_name}", "{last_name}")')
-            database.commit()
-            response['status'] = True
-            response['status_info'] = 'account created successfully',
-    return response
-
-
-@app.route('/api/login', methods=['POST'])
+@log.route('/api/login', methods=['POST'])
 def login():
     response = {}
     if request.method == 'POST':
@@ -88,18 +45,12 @@ def login():
 
     return response
 
+
 def encrypt_password(password_unencrypted, salt_string):
     encrypted_password = argon2.using(rounds=5, salt=salt_string).hash(password_unencrypted)
     return encrypted_password
 
-def generate_salt_string():
-    salt_string = bcrypt.gensalt()
-    return salt_string
 
 def generate_authorization_token():
     token_string = secrets.token_hex(32)
     return token_string
-    
-if __name__ == '__main__':
-    app.run(debug=True)
-
