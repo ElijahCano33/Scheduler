8a16170a81c009763c75c37eadedb10e3f80aa75
LeKinchi <lesporis@gmail.com>
2020-01-17 01:02:41 -0500

    updated requirements

--- BackEnd/BackEnd/__init__.py
+++ BackEnd/BackEnd/__init__.py
@@ -2,12 +2,15 @@ from flask import Flask, Blueprint
 from BackEnd.Routes.connection import connect
 from BackEnd.Routes.register import reg
 from BackEnd.Routes.login import log
+from BackEnd.Routes.friendship import friendship
+
 
 app = Flask(__name__)
 
 app.register_blueprint(connect)
 app.register_blueprint(reg)
 app.register_blueprint(log)
+app.register_blueprint(friendship)
 
 
 def RunServer():
