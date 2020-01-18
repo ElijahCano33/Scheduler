de96398cad2adc73d648cd7f882baf860b50d680
elijahcano33 <elijah.c104777@gmail.com>
2020-01-18 13:58:51 -0500

    Finished friend's list endpoint: returns correct response

--- BackEnd/BackEnd/Routes/friendsList.py
+++ BackEnd/BackEnd/Routes/friendsList.py
@@ -1,4 +1,4 @@
-from flask import Flask, Blueprint, request
+from flask import Flask, Blueprint, request, jsonify
 import mysql.connector
 from .login import userAuthenticationTracker
 
@@ -12,51 +12,43 @@ fl = Blueprint('friendsList', __name__)
 
 @fl.route('/api/friendsList', methods=['GET'])
 def fetchAllFriends():
-    response = {}
-    print("eheh")
-    #friends = []
+    response = []
+    friends = []
 
     if request.method == 'GET':
-        print("success")
 
         data = request.get_json()
         token = data['authentication token']
-        print("This is the token from frontend: ", token)
-        print("This is the login token from server: ", userAuthenticationTracker[0][1])
+        
 
-        #if token == userAuthenticationTracker[0][1]:
-            #print("tokens are equal")
+        if token == userAuthenticationTracker[0][1]:
+            email = userAuthenticationTracker[0][0]
+            cursor.execute(f"SELECT user_id FROM Scheduler.users WHERE email = '{email}'")
+            userIdQueryResult = cursor.fetchone()[0] 
+            cursor.execute(f"SELECT user_one_id FROM Scheduler.friendship WHERE user_two_id = '{userIdQueryResult}'")
+            friendsResult = cursor.fetchall() #[0][0] is for when there is only one friend
+            friendInfo = [tup[0] for tup in friendsResult]
+            
+            for row in friendInfo:
+                var = ("", row) 
+                friends.insert(0, var) 
+            
 
-            #email = userAuthenticationTracker[0][0]
-            #cursor.execute(f"SELECT user_id FROM Scheduler.users WHERE email = '{email}'")
-            #userIdQueryResult = cursor.fetchone()[0] #everything up to here works perfectly!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            #print("This is the user_id: ", userIdQueryResult) #everything up to here works perfectly!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-            #cursor.execute(f"SELECT user_one_id FROM Scheduler.friendship WHERE user_two_id = '{userIdQueryResult}'")
-            #friendsResult = cursor.fetchall() #this is where stuff starts breaking because not retriving data correctly
-            #for i in range(len(friendsResult)):
-            #    print(friendsResult[i])
-            #print("This is the first friends query result: ", friendsResult)
+            cursor.execute(f"SELECT user_two_id FROM Scheduler.friendship WHERE user_one_id = '{userIdQueryResult}'")
+            friendsResult = cursor.fetchall()
 
-            #for row in friendsResult:
-            #    friendInfo = ("", row) #gets the id of the friend
-            #    friends.insert(0, friendInfo) #stores the tuple in the friends list in 0(1)
-            #
-            #print("These are the friends: ", friends)
+            friendInfo = [tup[0] for tup in friendsResult]
 
-            #cursor.execute(f"SELECT user_two_id FROM Scheduler.friendship WHERE user_one_id = '{userIdQueryResult}'")
-            #friendsResult = cursor.fetchall()
+            
+            for info in friendInfo:
+                var = ("", info)
+                friends.insert(0, var)
 
-            #for row in friendsResult:
-            #    friendInfo = ("", row) #gets the id of the friend
-            #    friends.insert(0, friendInfo) #stores the tuple in the friends list in 0(1)
-
-            #for friendTuple in friendsResult:
-            #    friendId = friendTuple[1]
-            #    cursor.execute(f"SELECT first_name FROM Scheduler.users WHERE user_id = '{friendId}'")
-            #    friendFirstName = cursor.fetchall()
-            #    print("we got all the friends")
-            #    response[friendId] = friendFirstName
-    print("we out the if")
-    return response
+            for friendTuple in friends:
+                friendId = friendTuple[1]
+                cursor.execute(f"SELECT first_name FROM Scheduler.users WHERE user_id = '{friendId}'")
+                friendFirstName = cursor.fetchone()[0]
+                response.insert(0, friendFirstName)
+    return jsonify(response)
 
 
