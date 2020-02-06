dee432dfa5ec85dcf19d623633643af55a1ff62e
elijahcano33 <elijah.c104777@gmail.com>
2020-02-05 23:39:58 -0500

    changed navigation system from stack to switch

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -6,25 +6,7 @@ import LoadingScreen from './src/screens/LoadingScreen/Components/LoadingScreen.
 import LoginScreen from './src/screens/LoginScreen/Components/LoginScreen.js';
 import SignUpScreen from './src/screens/SignUpScreen/Components/SignUpScreen.js';
 import CalendarScreen from './src/screens/CalendarScreen/Components/CalendarScreen.js';
-import { fromLeft, fadeIn, zoomIn, zoomOut, flipX } from 'react-navigation-transitions'
  
-
-const handleCustomTransitionAnimations = ({ scenes }) => {
-  const prevScene = scenes[scenes.length - 2];
-  const nextScene = scenes[scenes.length - 1];
- 
-  // Custom transitions go there
-  if (prevScene
-    && prevScene.route.routeName === 'LoginScreen'
-    && nextScene.route.routeName === 'SignUpScreen') {
-    return fadeOut();
-  } else if (prevScene
-    && prevScene.route.routeName === 'SignUpScreen'
-    && nextScene.route.routeName === 'LoginScreen') {
-    return fromLeft();
-  }
-  return fadeIn();
-}
 
 /*
 The following establishes a network between 
@@ -33,7 +15,7 @@ so that only one screen is shown at a time. Sets
 the loading screen as the first screen to be displayed
 within the app.
 */
-const AppNavigator = createStackNavigator(
+const AppNavigator = createSwitchNavigator(
   {
     CalendarScreen: CalendarScreen,
     SignUpScreen: SignUpScreen,
@@ -43,7 +25,6 @@ const AppNavigator = createStackNavigator(
   {
     initialRouteName: 'LoadingScreen',
     headerMode: 'none',
-    transitionConfig: (nav) => handleCustomTransitionAnimations(nav)
   }
 );
 
