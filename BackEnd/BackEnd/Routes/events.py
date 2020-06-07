78bc9a06a851346d40d429a37a1640ebee7077c6
LeKinchi <lesporis@gmail.com>
2020-06-07 08:02:22 -0400

    Added getting events to Backend

--- BackEnd/BackEnd/Routes/events.py
+++ BackEnd/BackEnd/Routes/events.py
@@ -48,3 +48,54 @@ def createEvent():
     except Exception:
         return response, 400
     return response
+
+
+@event.route("/api/event", methods=['GET'])
+def GetEvent():
+    try:
+        database = mysql.connector.connect(host='scheduler-mysql-db.cxe7niamrusn.us-west-2.rds.amazonaws.com', database='Scheduler', user='admin_Scheduler', password='82h20kfaCrn05EKpEDrh')
+        if database:
+            cursor = database.cursor()
+            response = dict()
+            data = request.get_json()
+
+            userID = data["user_id"]
+            requestType = data["request_type"]
+
+            if requestType == "month":
+                month = data["month"]
+                year = data["year"]
+            elif requestType == "year":
+                year = data["year"]
+
+            if not userID or not requestType or not year:
+                error = "Can't create event because of missing essential parameters"
+                response['error'] = error
+                raise Exception(response)
+
+            if requestType == "month":
+                mySql_insert_query = """SELECT * FROM Scheduler.events where user_id = %s and month(startDate)= %s and year(startDate)= %s  """
+                recordTuple = (userID, month, year)
+                cursor.execute(mySql_insert_query, recordTuple)
+            elif requestType == "year":
+                mySql_insert_query = """SELECT * FROM Scheduler.events where user_id = %s and year(startDate)= %s  """
+                recordTuple = (userID, year)
+                cursor.execute(mySql_insert_query, recordTuple)
+            
+            eventList = cursor.fetchall()
+
+            if len(eventList) == 0:
+                    error = "No Events available for this period!"
+                    response['error'] = error
+                    raise Exception(response)
+
+            response['events'] = eventList
+
+        else:
+            error = "Connection to database failed!"
+            response['error'] = error
+            raise Exception(response)
+
+    except Exception:
+        return response, 400
+    return response
