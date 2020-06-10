38c69d552dda35bdda2411d5d059409fbd242f8c
elijahcano33 <elijah.c104777@gmail.com>
2020-06-10 14:40:09 -0400

    login screen code refactored

--- FrontEnd/src/screens/LoginScreen/Styles/LoginScreenStyles.js
+++ FrontEnd/src/screens/LoginScreen/Styles/LoginScreenStyles.js
@@ -1,71 +1,74 @@
 import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
-    screen: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: 'transparent'
-    },
-    fadeBackgroundStyles: {
-      justifyContent: 'center', 
-      alignItems: 'center', 
-      flex: 1
-    },
-  
-    logo: {
-      height: 650,
-      width: 800,
-      top: -220,//-35
-      left: '14%'
-    },
-  
-    input1: {
-      height: 40,
-      width: 250,
-      backgroundColor: '#FFFFFF',
-      paddingLeft: 10,
-      marginBottom: 20,
-      top: '35%',
-      position: 'absolute'
-    },
+  fadeBackground: {
+    justifyContent: 'center', 
+    alignItems: 'center', 
+    flex: 1
+  },
 
-    input2: {
-        height: 40,
-        width: 250,
-        paddingLeft: 10,
-        backgroundColor: '#FFFFFF',
-        marginBottom: 20,
-        top: '45%', 
-        position: 'absolute'
-      },
-  
-    buttonContainer1: {
+  logo: {
+    height: 650,
+    width: 800,
+    top: -220,//-35
+    left: '14%'
+  },
+
+  emailInput: {
+    height: 40,
+    width: 250,
+    backgroundColor: '#FFFFFF',
+    paddingLeft: 10,
+    marginBottom: 20,
+    top: '35%',
+    position: 'absolute'
+  },
+
+  passwordInput: {
+    height: 40, 
+    width: 250, 
+    backgroundColor: '#FFFFFF', 
+    marginBottom: 20, 
+    top: '45%', 
+    position: 'absolute'
+  },
+
+  loader: {
+    position: 'absolute', 
+    top: '-650%', 
+    left: '110%', 
+    width: '200%', 
+    height: '500%', 
+    backgroundColor: '#FF1DCE', 
+    borderRadius: 20
+  },
+
+  loginButton: {
+    width: 120,
+    backgroundColor: '#FF1DCE', //'#7ffff4
+    borderRadius: 20,
+    paddingHorizontal: 30,
+    textAlign: 'left',
+    left: '18%', //16% for it to work on all android screens
+    top: '54.5%',
+    position: 'absolute'
+  },
+
+  signUpButton: {
       width: 120,
-      backgroundColor: '#FF1DCE', //'#7ffff4
-      borderRadius: 20,
+      backgroundColor: '#FF1DCE',
       paddingHorizontal: 30,
+      borderRadius: 20,
       textAlign: 'left',
-      left: '18%', //16% for it to work on all android screens
       top: '54.5%',
+      left: '51%',
       position: 'absolute'
     },
 
-    buttonContainer2: {
-        width: 120,
-        backgroundColor: '#FF1DCE',
-        paddingHorizontal: 30,
-        borderRadius: 20,
-        textAlign: 'left',
-        top: '54.5%',
-        left: '51%',
-        position: 'absolute'
-      },
-  
-    buttonText: {
-      textAlign: 'center',
-      color: '#FFFFFF',
-      fontWeight: '700'
-    }
+  buttonText: {
+    textAlign: 'center',
+    color: '#FFFFFF',
+    fontWeight: '700'
+  }
   });
   
