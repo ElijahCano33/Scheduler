4f5c664c5bd6723a809db2addf36e8044b8f439d
elijahcano33 <elijah.c104777@gmail.com>
2020-06-11 13:18:36 -0400

    finished search screen refactoring

--- FrontEnd/src/screens/Search/Components/SearchScreen.js
+++ FrontEnd/src/screens/Search/Components/SearchScreen.js
@@ -1,11 +1,7 @@
 import React, {Component} from 'react';
-import { StyleSheet, TextInput, View, Text, Image, ImageBackground} from 'react-native';
+import { TextInput, View, Text, Image, ImageBackground} from 'react-native';
 import styles from '../Styles/SearchScreenStyles.js';
-import { SearchBar } from 'react-native-elements';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-
-//import SchedulerIconButtonsContainer from '../../../components/SchedulerIconButtonsContainer.js';
 
 export default class SearchScreen extends Component{
     constructor(props) {
@@ -23,9 +19,9 @@ export default class SearchScreen extends Component{
     };
     
     render() {
-        const { search } = this.state;
         return (
-            <ImageBackground source={require('../../../../pics/fade.jpg')} style={styles.fadeBackgroundStyles}>
+            <ImageBackground source={require('../../../../pics/fade.jpg')} style={styles.fadeBackground}>
+
                 <Image
                     style={styles.logo}
                     source={require('../../../../pics/scriptscheduler.png')}
@@ -36,11 +32,13 @@ export default class SearchScreen extends Component{
                     style={styles.searchBar}
                     onChangeText={(search) => this.setState({search})}
                 />
-                <FontAwesome name="search" color={'white'} size={25} style={{top: '20.85%', left: '5.5%', position: 'absolute'}} />
 
-                {this.state.unfilteredData.length === 0 ? 
-                    <View style={{top: '37.5%', left: '8%', position: 'absolute', height: '10%', width: '50%'}}>
-                        <FontAwesome name="search" color={'#2f4f4f'} size={200} style={{top: '50%', left: '40%', position: 'absolute'}} />
+                <FontAwesome name="search" color={'white'} size={25} style={styles.searchIcon} />
+
+                {this.state.unfilteredData.length === 0 ?
+
+                    <View style={styles.noSearchResultsView}>
+                        <FontAwesome name="search" color={'#2f4f4f'} size={200} style={styles.noSearchResultsIcon} />
                         <Text style={styles.noSearchResults}>No Current Search Results</Text>
                     </View>
                 
