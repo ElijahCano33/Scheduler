964c0626a4789afbcb533f36f77dc5dc66735566
elijahcano33 <elijah.c104777@gmail.com>
2020-06-11 14:14:31 -0400

    friend screen code refactored

--- FrontEnd/src/screens/Friends/Components/FriendBox.js
+++ FrontEnd/src/screens/Friends/Components/FriendBox.js
@@ -2,13 +2,9 @@ import React, {Component} from 'react';
 import { View,Text, Image } from "react-native";
 import styles from '../Styles/FriendBoxStyles.js';
 
-
-
 export default class FriendBox extends Component{
-    
     render() {
         return (
-            
             <View style={styles.container}>
                 <Text style={styles.firstName}>{this.props.firstName}</Text>
                 {this.props.firstName.length <= 3 ? <Text style={styles.lastName}>{this.props.lastName}</Text> : <Text style={styles.lastName2}>{this.props.lastName}</Text>}
--- FrontEnd/src/screens/Friends/Styles/FriendsScreenStyles.js
+++ FrontEnd/src/screens/Friends/Styles/FriendsScreenStyles.js
@@ -1,25 +1,20 @@
 import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
-    fadeBackgroundStyles: {
+    fadeBackground: {
         justifyContent: 'center', 
         alignItems: 'center', 
-        //flexDirection: 'row',
         flex: 1
     },
+
     logo: {
         height: 650,
         width: 800,
-        top: -210,//-35
+        top: -210,
         left: '-37%',
         position: 'absolute'
     },
-    container: {
-        width: 395,
-        height: 110,
-        bottom: '-12%',
-        backgroundColor: 'red'
-    },
+
     searchBar: {
         height: 40,
         width: 368,
@@ -33,4 +28,33 @@ export default StyleSheet.create({
         position: 'absolute',
         fontWeight: 'bold'
     },
+
+    searchBarIcon: {
+        top: '20.85%', 
+        left: '5.5%', 
+        position: 'absolute'
+    },
+
+    sadFaceIcon: {
+        top: '35%', 
+        left: '-4%', 
+        position: 'absolute'
+    },
+
+    noFriendsText: {
+        top: '68%', 
+        fontWeight: 'bold', 
+        color: 'white'
+    },
+
+    friendsList: {
+        top: '28%', 
+        marginBottom: 0, 
+        bottom: 50, 
+        width: '100%', 
+        height: '80%', 
+        backgroundColor: 'transparent', 
+        position: 'absolute'
+    },
+
 });
