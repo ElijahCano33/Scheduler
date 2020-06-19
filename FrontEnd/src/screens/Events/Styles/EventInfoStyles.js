ecefb768a59e192af0fd06485cd0a4847312591a
elijahcano33 <elijah.c104777@gmail.com>
2020-06-19 16:18:54 -0400

    events screen completed

---
+++ FrontEnd/src/screens/Events/Styles/EventInfoStyles.js
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    eventContainer: {
+        paddingBottom: 10, 
+        borderBottomWidth: 5, 
+        borderColor: 'pink'
+    },
+
+    text: {
+        fontSize: 15, 
+        fontWeight: 'bold', 
+        color: 'white', 
+        fontFamily: 'sans-serif-thin'
+    }
+});
--- FrontEnd/src/screens/Events/Styles/EventScreenStyles.js
+++ FrontEnd/src/screens/Events/Styles/EventScreenStyles.js
@@ -2,8 +2,29 @@ import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
     container: {
-        flex: 1,
-        backgroundColor: '#2f4f4f',
+        backgroundColor: '#2d2d2d', 
+        flex: 1, 
+        justifyContent: 'center'
+    },
+
+    eventScreenHeaderText: {
+        position: 'absolute',
+        top: '0%',
+        left: '15%',
+        fontWeight: 'bold',
+        fontFamily: 'sans-serif-thin',
+        color: 'white',
+        fontSize: 25
+    },
+
+    icon: {
+        position: 'absolute', 
+        top: '0.5%', 
+        left: '0%'
+    },
+
+    flatList: {
         justifyContent: 'center', 
+        alignItems: 'center'
     }
 });
