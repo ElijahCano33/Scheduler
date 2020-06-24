8f7f4e05f8efa229d31ef125d8a948caa8887776
elijahcano33 <elijah.c104777@gmail.com>
2020-06-24 17:56:06 -0400

    fixed re-render issue with calendar

--- FrontEnd/App.js
+++ FrontEnd/App.js
@@ -1,14 +1,27 @@
 import {createSwitchNavigator, createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 import LoadingScreen from './src/screens/LoadingScreen/Components/LoadingScreen.js';
 import LoginScreen from './src/screens/LoginScreen/Components/LoginScreen.js';
 import SignUpScreen from './src/screens/SignUpScreen/Components/SignUpScreen.js';
 import CalendarScreen from './src/screens/CalendarScreen/Components/CalendarScreen.js';
 import EventScreen from './src/screens/Events/Components/EventScreen.js';
  
+const CalendarNavigator = createStackNavigator(
+  {
+      CalendarScreen: CalendarScreen,
+      EventScreen: EventScreen
+
+  }, 
+  {
+    initialRouteName: 'CalendarScreen',
+    headerMode: 'none',
+  }
+);
+
+
 const AppNavigator = createSwitchNavigator(
   {
-    EventScreen: EventScreen,
-    CalendarScreen: CalendarScreen,
+    CalendarScreen: CalendarNavigator,
     SignUpScreen: SignUpScreen,
     LoginScreen: LoginScreen,
     LoadingScreen: LoadingScreen
